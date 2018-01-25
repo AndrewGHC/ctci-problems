811095a8e60112115335fb8a7a3679ef6fd8a6a4
AndrewGHC <drewwalsh1@gmail.com>
2018-01-25 22:09:23 +0000

    C - String has unique chars

---
+++ Chapter-1_Arrays_and_Strings/1.1_string_has_unique_chars/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Determine if a string of max length 64 chars has unique characters
+
+int strIsUnique(char *str)
+{
+   int ascii[255];
+   for (int i = 0; i < strlen(str); i++) {
+     if (ascii[(int)str[i]] == 0) {
+       ascii[(int)str[i]] = 1;
+     } else {
+       printf("%s\n", "Not unique");
+       return 1;
+     }
+   }
+   printf("%s\n", "Unique");
+   return 0;
+}
+
+int main(int argc, char* argv[]) {
+  char inputString[64];
+  scanf("%s", inputString);
+  return strIsUnique(inputString);
+}
