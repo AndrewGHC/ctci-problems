58946e8c2d51e0a50d1bf3c22904bc061aebac15
AndrewGHC <drewwalsh1@gmail.com>
2018-02-18 23:45:49 +0000

    Check palindromes in C

---
+++ Chapter-1_Arrays_and_Strings/1.4_string_is_a_palindrome/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Check if a string is a palindrome
+int main(int argc, char *argv[])
+{
+  if (argc == 1) {
+    printf("No string passed");
+    return 1;
+  } else if (argc >= 3) {
+    printf("This program only accepts one argument");
+    return 1;
+  }
+
+  char *str = argv[1];
+  char *reversedStr = (char *) malloc(strlen(str));
+
+  for (int i = strlen(str); i >= 0; i--) {
+    strncat(reversedStr, &str[i], 1);
+  }
+
+  if (strcmp(str, reversedStr) == 0) {
+    printf("%s is a palindrome", str);
+    return 0;
+  }
+
+  printf("%s is not a palindrome", str);
+  return 1;
+}
